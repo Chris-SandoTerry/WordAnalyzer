a00929dc275d420ecc5d0197b886c055902f4b87
Christopher Sandoval Terry <115430496+Chris-SandoTerry@users.noreply.github.com>
2024-08-29 13:27:51 -0500

    Renaming Project

--- WordAnalyzer/Main.cpp
+++ WordAnalyzer/Main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 int main()
 {
-    WordCollection w("C:/Users/Gamin/source/repos/Redo Project 2/1984.txt");
+    WordCollection w("C:/Users/Gamin/source/repos/WordAnalyzer/1984.txt");
     w.printAllWords();
 
     return 0;
